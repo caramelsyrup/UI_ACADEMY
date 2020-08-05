46be60a1ed6960b7c592385abd8b497b68874d95
caramelsyrup <seojunlim87@gmail.com>
2020-08-05 23:28:32 +0900

    first

---
+++ HTML/js/introduce.js
@@ -0,0 +1,7 @@
+function showDetail() {
+    document.querySelector('#content').style.display = "block";
+}
+
+function hideDetail() {
+    document.querySelector('#content').style.display = "none";
+}
